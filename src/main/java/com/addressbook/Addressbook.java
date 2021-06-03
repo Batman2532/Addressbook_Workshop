84f6b7c1276c4ccc8848aaa7d6f9d588c8b5e934
Batman2532 <charjan44@gmail.com>
2021-06-03 11:46:01 +0530

    Added: add contacts

--- src/main/java/com/addressbook/Addressbook.java
+++ src/main/java/com/addressbook/Addressbook.java
@@ -1,7 +1,67 @@
 package com.addressbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Addressbook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to addressbook system");
+        public static String firstName;
+        public static String lastName;
+        public static String address;
+        public static String city;
+        public static String state;
+        public static int zipCode;
+        public static int phoneNumber;
+        public static String email;
+
+        public Addressbook(String firstName, String lastName, String address, String city, String state, int zipCode, int phoneNumber, String email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public void add() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter First Name");
+            this.firstName = sc.nextLine();
+            System.out.println("Enter Last Name");
+            this.lastName = sc.nextLine();
+            System.out.println("Enter Address");
+            this.address = sc.nextLine();
+            System.out.println("Enter City");
+            this.city = sc.nextLine();
+            System.out.println("Enter State");
+            this.state = sc.nextLine();
+            System.out.println("Enter Zipcode");
+            this.zipCode = sc.nextInt();
+            System.out.println("Enter Phone Number");
+            this.phoneNumber = sc.nextInt();
+            System.out.println("Enter Email");
+            this.email = sc.next();
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Address Book System");
+            ArrayList<Addressbook> contacts = new ArrayList<>();
+            Addressbook ab = new Addressbook(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+            System.out.println("Enter 1 to add contact: ");
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    ab.add();
+                    ab.toString();
+                    contacts.add(ab);
+                    System.out.println(contacts.get(0));
+                    break;
+                default:
+                    System.out.println("Enter valid number");
+                    break;
+            }
+        }
     }
-}
+
